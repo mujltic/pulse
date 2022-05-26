e4ec460b3ca1c636cbb169579be8338ed7b7018c
mujltic <mujltic@gmail.com>
2022-05-27 02:54:00 +0600

    slick

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/Arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+        
+    });
+   
+              
+  });
+
+
