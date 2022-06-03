18f65e1b0004186b8e47b2619f0207f112c0886c
mujltic <mujltic@gmail.com>
2022-06-03 22:05:22 +0600

    slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/Arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next">Next</button>',
-        
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/Arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows:false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows:false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows:false
+              }
+            }
+          ]
     });
-   
-              
-  });
-
-
+});
