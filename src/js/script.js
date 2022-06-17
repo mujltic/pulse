619a2603a0e6ca6ec4bbff2737ee58b4585af51a
mujltic <mujltic@gmail.com>
2022-06-17 14:54:08 +0600

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,43 @@
-$(document).ready(function(){
+var slider = tns({
+  container: '.carousel__slide',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls:false,
+  nav: false,
+  autoHeight:true,
+  responsive: {
+    575: {
+      nav: false,
+    },
+    320: {
+      nav: true,
+    }
+     }
+});
+
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+document.querySelector('.next').onclick = function () {
+  slider.goTo('next');
+};
+
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/Arrow_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
         responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 575,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                infinite: true,
                 dots: true,
                 arrows:false
               }
             },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-                arrows:false
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-                arrows:false
-              }
-            }
           ]
     });
-});
+}); slick slider */
+
